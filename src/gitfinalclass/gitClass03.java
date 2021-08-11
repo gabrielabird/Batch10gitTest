cfff3cbf7c08393f625f70da99ffaf6bd310e7c4
gabri <bird.gs88@gmail.com>
2021-08-11 19:36:11 -0400

    the first commit of project

---
+++ src/gitfinalclass/gitClass03.java
@@ -0,0 +1,12 @@
+package gitfinalclass;
+
+public class gitClass03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is my first commit file");
+
+	}
+
+}
