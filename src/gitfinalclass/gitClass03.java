0537e2b1c97d6ec68340db456dbf767ac5d71928
gabri <bird.gs88@gmail.com>
2021-08-11 21:24:09 -0400

    third commit

--- src/gitfinalclass/gitClass03.java
+++ src/gitfinalclass/gitClass03.java
@@ -5,7 +5,7 @@ public class gitClass03 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("this is my first commit file");
+		System.out.println("this is the none commit file");
 
 	}
 
